37569aef87d5870fc079824391151e45186160f7
mikaeltreppo <mikaeltreppo@outlook.com>
2023-03-14 14:03:21 +0100

    Inizio aggiunta immagine

--- js/main.js
+++ js/main.js
@@ -3,7 +3,8 @@ const { createApp } = Vue
   createApp({
     data() {
       return {
-        message: "Ciao Mondo!!!"
+        message: "Ciao Mondo!!!",
+        immagine : "https://tse3.mm.bing.net/th?id=OIP.KYsS-IQ5v_OZAOEzgJB74AHaFG&pid=Api&P=0"
       }
     }
   }).mount('#app')
